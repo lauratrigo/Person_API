5e138eda2ad85b9ed1c455040bd813fd2d07d790
Laura Trigo <160795749+lauratrigo@users.noreply.github.com>
2025-02-09 15:14:48 -0300

    Add files via upload

---
+++ models/PersonModel.cs
@@ -0,0 +1,20 @@
+public class PersonModel {
+
+    public PersonModel (string name) {
+
+        Name = name;
+        Id = Guid.NewGuid();
+    }
+    public Guid Id {get; init;}
+    public string Name {get; private set;}
+
+    public void  ChangeName (string name) {
+        Name = name;
+    }
+
+    public void SetInactive() {
+
+        Name = "Desativado";
+    }
+
+}
